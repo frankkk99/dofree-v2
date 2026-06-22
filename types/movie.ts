6c46e81006e400bdb3b3157c22941bf88e6725ad
Frank chetsarit <am2.production29@gmail.com>
2026-06-23 00:44:35 +0700

    Add movie type

---
+++ types/movie.ts
@@ -0,0 +1,12 @@
+export type Movie = {
+  id: number;
+  title: string;
+  thaiTitle?: string;
+  year: string;
+  rating: string;
+  genres: string[];
+  overview: string;
+  poster: string;
+  backdrop: string;
+  trailerUrl?: string;
+};
