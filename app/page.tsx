2654d61d46f671b6d0c4fcdcdc606896068c4dc5
Frank chetsarit <am2.production29@gmail.com>
2026-06-23 10:17:53 +0700

    Use three row expandable sections on home

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import { ExperienceShellThaiCategories } from '@/components/ExperienceShellThaiCategories';
+import { ExperienceShellThaiCategoriesRows } from '@/components/ExperienceShellThaiCategoriesRows';
 import { MouseDragCarouselEnhancer } from '@/components/MouseDragCarouselEnhancer';
 
 export default function HomePage() {
   return (
     <main>
-      <ExperienceShellThaiCategories />
+      <ExperienceShellThaiCategoriesRows />
       <MouseDragCarouselEnhancer />
     </main>
   );
